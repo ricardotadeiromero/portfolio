e05d01063074b48d6bee623e28d8734456eda55f
ricardo.romero <99834981+ricardotadeiromero@users.noreply.github.com>
2023-11-14 20:51:35 +0000

    Refactor Navbar component and add NavLink component

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
+import { NavLink } from "./NavLink";
+
+const Links = [
+  {
+    href: "#About",
+    title: "About",
+  },
+  {
+    href: "#Projects",
+    title: "Projects",
+  },
+  {
+    href: "#Contact",
+    title: "Contact",
+  },
+];
+
 export const Navbar = () => {
   return (
-    <nav>
-        <div className="flex flex-wrap items-center justify-between mx-auto p-8">
-            <Link href={"/"} className='text-5xl text-white font-semibold'>LOGO</Link>
+    <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90">
+      <div className="flex flex-wrap items-center justify-between mx-auto p-4">
+        <Link
+          href={"/"}
+          className="text-2xl md:text-5xl text-white font-semibold"
+        >
+          LOGO
+        </Link>
+        <div className="menu">
+          <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
+            {Links.map((link, index) => {
+              return (
+                <li key={index}>
+                  <NavLink href={link.href} title={link.title} />
+                </li>
+              );
+            })}
+          </ul>
         </div>
+      </div>
     </nav>
-  )
-}
+  );
+};
--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
       <Navbar />
-      <container className="container mx-auto px-12 py-4">
+      <container className="container mx-auto px-12 py-4"> 
         <HeroSection />
       </container>
     </main>
